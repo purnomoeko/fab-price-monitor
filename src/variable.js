a20764dd03dcc7c1695624cf1526e20d583dc8df
Eko Purnomo <eko.purnomo@icloud.com>
2019-02-17 00:47:57 +0700

    Update APIURL

--- src/variable.js
+++ src/variable.js
@@ -1,4 +1,4 @@
-export const APIURL = __DEV__ ? 'http://localhost:3000/' : 'http://test.daurulang.id/';
+export const APIURL = __DEV__ ? 'http://localhost:3000/' : 'http://test.rekamjejak.com:4000/';
 export const MediaUrl = productId => `https://fabelio.com/swatches/ajax/media/?product_id=${productId}&attributes%5B`;
 
 export default null;
